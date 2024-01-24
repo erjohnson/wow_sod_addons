97ba5e44bf507cb1aee63e1f69c3fe0a4819fbae
Eric Johnson <erjohnson.pdx@gmail.com>
2024-01-23 16:06:26 -0800

    [fix] revert to hard-coded value

--- hello_uiscale.lua
+++ hello_uiscale.lua
@@ -1,10 +1,9 @@
 -- https://warcraft.wiki.gg/wiki/UI_scaling
 -- https://warcraft.wiki.gg/wiki/API_Region_SetScale
-local width, height = GetPhysicalScreenSize()
 local f = CreateFrame("Frame", nil, UIParent)
 f:RegisterEvent("PLAYER_ENTERING_WORLD")
 f:SetScript("OnEvent", function(self, event)
-	UIParent:SetScale(height/width)
+	UIParent:SetScale(0.5333)
 	f:UnregisterAllEvents()
 end)
 
